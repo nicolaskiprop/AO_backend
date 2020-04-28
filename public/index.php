7ca7b9b437ea3711c3721241ab7a796d7f79827d
nick <14333454+nicolaskiprop@users.noreply.github.com>
2020-04-28 22:13:42 +0300

    run commented out

--- public/index.php
+++ public/index.php
@@ -46,4 +46,13 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$app->run();
+//$app->run();
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
